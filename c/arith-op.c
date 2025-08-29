82f4053a4a2d7ec5f8f55bf646725fd2bccfe620
isaac-rom3ro <is4ac.romero@outlook.com>
2025-08-28 22:10:55 -0300

    update/ c: starting operators | python: executable programs, venv

---
+++ c/arith-op.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+  int a = 1;
+  int b = 1;
+
+  float x = 10;
+  float y = 5;
+  
+  float result = 5.0 / 3;
+
+  printf("%f\n", x / y); // Even if the variable is set to float, the / as standard executes integer operations  
+  printf("%f", result); // in order to change it, not truncate the decimal part,
+
+
+  return 0;
+}
